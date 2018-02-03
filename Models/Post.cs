c2d23c26ff63b634469e5f8f0b2ac78d2cd58be8
The Genius <rapgeek@gmail.com>
2018-02-02 23:05:54 -0600

    Added the Models folder to the projec to make it more powerful

---
+++ Models/Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace RapGeekBlog.Models
+{
+    public class Post
+    {
+        public int PostId
+        {
+            get;
+            set;
+        }
+
+        public string PostTitle
+        {
+            get;
+            set;
+        }
+        public string Body
+        {
+            get;
+            set;
+        }
+        public int CategoryId
+        {
+            get;
+            set;
+        }
+        public DateTime CreateDate
+        {
+            get;
+            set;
+        }
+        public int AuthorId
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace RapGeekBlog.Models
+{
+    public class User
+    {
+        public int UserId
+        {
+            get;
+            set;
+        }
+
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        public string PasswordHash
+        {
+            get;
+            set;
+        }
+
+        public string Firstname
+        {
+            get;
+            set;
+        }
+
+        public string Lastname { get; set; }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public DateTime Created { get; set; }
+    }
+}
